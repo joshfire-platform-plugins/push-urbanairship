5b05df5dca3737b478fbce7d6010553ba55fcca7
Thibaud Cummings <thibaud.cummings@gmail.com>
2013-06-20 10:01:21 +0200

    initial commit

---
+++ androidproj.js
@@ -0,0 +1,228 @@
+define(['woodman'], function (woodman) {
+  return function (runtime, params, callback) {
+
+    var logger = woodman.getLogger('add-on', 'push', 'androidproj');
+    logger.log('started');
+
+    // should use path.sep rather than '/' ... don't want this to fail on MSWIN
+    var packagePath = params.packagename.replace(/\./g, '/');
+
+    /**
+     * Invoke plugman.install on the android project
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function plugmanInstall(cb) {
+      runtime.plugmanInstall('./urban-airship', function (err) {
+        if (err) {
+          logger.error('plugmanInstall error', err);
+        } else {
+          logger.log('we DONE');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Injects source code in MyApplication.java at various placeholders:
+     *  - imports
+     *  - onCreate
+     *  - onStop
+     *
+     * TODO: prepend rather than replace the placeholders so we can potentially
+     * inject more code.
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function injectInApplicationSrc(cb) {
+      // 4 space indent
+      var indent = '    ';
+
+      var imports = [
+        'import com.urbanairship.UAirship;',
+        'import com.urbanairship.phonegap.plugins.PushNotificationPluginIntentReceiver;',
+        'import com.urbanairship.push.PushManager;'
+      ].join('\n');
+
+      var onCreate = [
+        'UAirship.takeOff(this);',
+        'if (UAirship.shared().getAirshipConfigOptions().pushServiceEnabled) {',
+        indent + 'PushManager.enablePush();',
+        indent + 'PushManager.shared().setIntentReceiver(PushNotificationPluginIntentReceiver.class);',
+        '}'
+      ].map(function (s, i) {
+        return (i ? indent + indent : '') + s;
+      }).join('\n');
+
+      var onStop = 'UAirship.land();';
+
+      var replaceMap = {
+        '\\/\\*\\*___JOSHFIRE_IMPORT_PLACEHOLDER___\\*\\*\\/'   : imports,
+        '\\/\\*\\*___JOSHFIRE_ONCREATE_PLACEHOLDER___\\*\\*\\/' : onCreate,
+        '\\/\\*\\*___JOSHFIRE_ONSTOP_PLACEHOLDER___\\*\\*\\/'   : onStop
+      };
+
+      // we should do something like 'prepend' so we can inject more...
+
+      logger.log('injectInApplicationSrc replaceMap', replaceMap);
+
+      var fpath = './src/' + packagePath + '/MyApplication.java';
+      logger.log('injectInApplicationSrc fpath', fpath);
+
+      runtime.multipleReplaceInFile(fpath, replaceMap, function (err) {
+        if (err) {
+          logger.warn('injectInApplicationSrc multipleReplaceInFile error', err);
+        } else {
+          logger.log('injectInApplicationSrc OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Copies the Urban Airship config file (airshipconfig.properties)
+     * to the android project (in assets directory)
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function copyUAConfigFileToProject(cb) {
+      var UAplistFile = './urban-airship/android/assets/airshipconfig.properties.sample';
+      var UAplistDestFile = './assets/airshipconfig.properties';
+
+      runtime.copyFromAddon(UAplistFile, UAplistDestFile,  function (err) {
+        if (err) {
+          logger.warn('copyUAConfigFileToProject copyFromAddon error', err);
+        } else {
+          logger.log('copyUAConfigFileToProject OK!');
+        }
+
+        cb(err, UAplistDestFile);
+      });
+    }
+
+    /**
+     * Updates the Urban Airship config file (airshipconfig.properties)
+     *
+     * @function
+     * @param {string}   configFilePath The path to the config file (relative
+     *  to the android projects' directory)
+     * @param {function} cb Callback
+     */
+    function updateUAConfigFile(configFilePath, cb) {
+      var envtype = params.envtype;
+      //if (!envtype) envtype = 'debug';
+      envtype = 'debug';
+      logger.log('updateUAConfigFile envtype', envtype);
+      var dev = true;
+
+      if (envtype === 'live' || envtype === 'staging') {
+        dev = false;
+      } else if (envtype === 'debug') {
+        dev = true;
+      } else {
+        var err = new Error('envtype ' + envtype + ' is not recognized');
+        logger.warn('updateUAConfigFile error', err);
+        cb(err);
+        return;
+      }
+
+      var prefix = dev ? 'development' : 'production';
+      var replaceMap = {};
+      replaceMap['__gcmSender__']               = '633609568080';
+      replaceMap['__' + prefix + 'AppKey__']    = 'hVN_js8kSBygQCYMK4wN8w';
+      replaceMap['__' + prefix + 'AppSecret__'] = '9PRDQ1w9SdO__vkxqybeSw';
+      replaceMap['__inProduction__']            = dev ? 'false' : 'true';
+
+      // inproduction...
+
+      logger.log('updateUAConfigFile replaceMap', replaceMap);
+
+      runtime.multipleReplaceInFile(configFilePath, replaceMap, function (err) {
+        if (err) {
+          logger.warn('updateUAConfigFile multipleReplaceInFile error', err);
+        } else {
+          logger.log('updateUAConfigFile OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Update the PushNotificationPluginIntentReceiver class with the projects'
+     * main activity name
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function updateSrcFile(cb) {
+      var replaceMap = {
+        '___JOSHFIREFACTORYACTIVITYNAME___' : params.activityname
+      };
+
+      var srcFilePath = './src/com/urbanairship/phonegap/plugins/PushNotificationPluginIntentReceiver.java';
+
+      logger.log('updateSrcFile srcFilePath', srcFilePath);
+      logger.log('updateSrcFile replaceMap', replaceMap);
+
+      runtime.multipleReplaceInFile(srcFilePath, replaceMap, function (err) {
+        if (err) {
+          logger.warn('updateSrcFile multipleReplaceInFile error', err);
+        } else {
+          logger.log('updateSrcFile OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Update the Urban Airship config.
+     * first copy the config file it to the project then update it.
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function updateUAConfig(cb) {
+      runtime.async.waterfall([
+        copyUAConfigFileToProject,
+        updateUAConfigFile
+      ], function (err) {
+        if (err) {
+          logger.warn('updateUAConfig async.waterfall error', err);
+        } else {
+          logger.log('updateUAConfig OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * the last three methods need plugman.install to have run (for all the files
+     * to be available in the android project)
+     * these last three methods could be executed in parallel but a single async.series
+     * is better for simplicity
+     */
+    runtime.async.series([
+      plugmanInstall,
+      injectInApplicationSrc,
+      updateSrcFile,
+      updateUAConfig
+    ], function (err) {
+      if (err) {
+        logger.warn('main async.series error', err);
+      } else {
+        logger.log('DONE');
+      }
+
+      callback(err);
+    });
+
+  };
+});
---
+++ xcodeproj.js
@@ -0,0 +1,157 @@
+define(['woodman'], function (woodman) {
+  return function (runtime, params, callback) {
+
+    var logger = woodman.getLogger('add-on', 'push', 'xcodeproj');
+    logger.log('started');
+
+    /**
+     * Invoke plugman.install on the xcode project
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function plugmanInstall(cb) {
+      runtime.plugmanInstall('./urban-airship', function (err) {
+        if (err) {
+          logger.error('plugmanInstall error', err);
+        } else {
+          logger.log('we DONE');
+        }
+
+        cb(err);
+      });
+    }
+
+
+    /**
+     * Copy the Urban Airship library to the xcode projects' app directory
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function copyLibraryToProject(cb) {
+      var iOSLibrary = './urban-airship/ios/lib';
+      var libDest = './Airship';
+
+      runtime.copyFromAddonToProject(iOSLibrary, libDest, function (err) {
+        if (err) {
+          logger.warn('copyLibraryToProject copyFromAddonToProject error', err);
+        } else {
+          logger.log('copyLibraryToProject OK!');
+        }
+
+        cb(err, libDest);
+      });
+    }
+
+    /**
+     * Updates xcode projects' header search paths with the Urban Airship library
+     *
+     * @function
+     * @param {string}   libDest The path to the library (relative
+     *  to the xcode projects' app directory)
+     * @param {function} cb Callback
+     */
+    function updateHeaderSearchPaths(libDest, cb) {
+      runtime.addHeaderSearchPath(libDest, true, function (err) {
+        if (err) {
+          logger.warn('updateHeaderSearchPaths addHeaderSearchPath error', err);
+        } else {
+          logger.log('updateHeaderSearchPaths OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Adds the Urban Airship library to the projects' header search path
+     * first copy the lib to the xcode projects' app directory then update
+     * the header search paths.
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function addLibraryToHeaderSearchPaths(cb) {
+      runtime.async.waterfall([
+        copyLibraryToProject,
+        updateHeaderSearchPaths
+      ], function (err) {
+        if (err) {
+          logger.warn('addLibraryToHeaderSearchPaths async.waterfall error', err);
+        } else {
+          logger.log('addLibraryToHeaderSearchPaths OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * Updates the Urban Airship config file (Resources/AirshipConfig.plist)
+     *
+     * @function
+     * @param {string}   configFilePath The path to the config file (relative
+     *  to the xcode projects' app directory)
+     * @param {function} cb Callback
+     */
+    function updateUAConfig(cb) {
+      var envtype = params.envtype;
+      //if (!envtype) envtype = 'debug';
+      envtype = 'debug';
+      logger.log('updateUAConfig envtype', envtype);
+      var dev = true;
+
+      if (envtype === 'live' || envtype === 'staging') {
+        dev = false;
+      } else if (envtype === 'debug') {
+        dev = true;
+      } else {
+        var err = new Error('envtype ' + envtype + ' is not recognized');
+        logger.warn('updateUAConfig error', err);
+        cb(err);
+        return;
+      }
+
+      var prefix = dev ? 'DEVELOPMENT' : 'PRODUCTION';
+      var replaceMap = {};
+      replaceMap['__APP_STORE_OR_AD_HOC_BUILD__'] = dev ? 'NO' : 'YES';
+      replaceMap['__' + prefix + '_APP_KEY__']    = '"hVN_js8kSBygQCYMK4wN8w"';
+      replaceMap['__' + prefix + '_APP_SECRET__'] = '"9PRDQ1w9SdO__vkxqybeSw"';
+
+      logger.log('updateUAConfig replaceMap', replaceMap);
+
+      var configFilePath = './Resources/AirshipConfig.plist';
+
+      runtime.multipleReplaceInFile(configFilePath, replaceMap, function (err) {
+        if (err) {
+          logger.warn('updateUAConfig multipleReplaceInFile error', err);
+        } else {
+          logger.log('updateUAConfig OK!');
+        }
+
+        cb(err);
+      });
+    }
+
+    /**
+     * the last two methods need plugman.install to have run (for all the files
+     * to be available in the xcode project)
+     * these last two methods could be executed in parallel but a single async.series
+     * is better for simplicity
+     */
+    runtime.async.series([
+      plugmanInstall,
+      addLibraryToHeaderSearchPaths,
+      updateUAConfig
+    ], function (err) {
+      if (err) {
+        logger.warn('main async.series error', err);
+      } else {
+        logger.log('DONE');
+      }
+
+      callback(err);
+    });
+  };
+});
